4256013da19ee35f64f8641f82809c73757b1ad9
mahima-c <54117469+mahima-c@users.noreply.github.com>
2019-10-21 21:38:36 +0530

    Update urls.py

--- akggram/insta/auth/urls.py
+++ akggram/insta/auth/urls.py
@@ -3,8 +3,8 @@ from insta.auth import views
 
 
 urlpatterns = [
-    url(r'^me/?$', views.UserView.as_view(),name='user'),
-    url(r'^me/profile/?$', views.ProfileView.as_view(),name='profile'),
+    #url(r'^me/?$', views.UserView.as_view(),name='user'),
+    #url(r'^me/profile/?$', views.ProfileView.as_view(),name='profile'),
     url(r'^login/?$', views.Loginview.as_view(),name='login'),
     url(r'^logout/?$', views.LogoutView.as_view(),name='logout'),
     url(r'^register/?$', views.RegisterView.as_view(),name='register'),
