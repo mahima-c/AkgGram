9e72aa4e14b2f32ecdc2d0ecdb19e53bf9888d4d
mahima-c <13as1827000713@gmail.com>
2019-10-30 16:00:00 +0530

    updated

--- akggram/akggram/settings.py
+++ akggram/akggram/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 AUTH_USER_MODEL='insta.User'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 '''REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
--- akggram/insta/models.py
+++ akggram/insta/models.py
@@ -4,40 +4,104 @@ from phonenumber_field.modelfields import PhoneNumberField
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.core.validators import RegexValidator
+import datetime
+from django.conf import settings
+
+from django.contrib.humanize.templatetags.humanize import naturaltime
+
+
+  
+import uuid
+import os
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
+
+
+def image_file_path(instance, filename):
+    """Generate file path for new recipe image"""
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('uploads/', filename)
+
 
 # Create your models here.
 
-
-
-'''class Role(models.Model):
-    """
-    this is defined to allot a role to a user.
-    """
-    id = models.AutoField(primary_key=True)
-    role = models.CharField(max_length=20,unique=True,default="user")
-
-    def __str__(self):
-        return '%s'%(self.role)
-'''
-
 class User(AbstractUser):
-    """
-    user is customised and related to model Role using Abstract user.
-    """
+    
     username=models.CharField(max_length=200,unique=True)
     email=models.EmailField(max_length=200,unique=True,help_text='Required')
     first_name=models.CharField(max_length=200)
     last_name=models.CharField(max_length=200)
+    profile_image = models.ImageField(null=True)
+    website = models.URLField(null=True)
+    bio = models.TextField(null=True)
+    followers = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                       related_name="user_followers",
+                                       blank=True,
+                                       symmetrical=False)
+    following = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                       related_name="user_following",
+                                       blank=True,
+                                       symmetrical=False)
 
+    def number_of_followers(self):
+        if self.followers.count():
+            return self.followers.count()
+        else:
+            return 0
+
+    def number_of_following(self):
+        if self.following.count():
+            return self.following.count()
+        else:
+            return 0
+
+    def __str__(self):
+            return self.username
+    
 
 
 class OTP(models.Model):
-    """
-    Model to store Otp of user And verify user.
-    """
     receiver = models.OneToOneField(User, on_delete=models.CASCADE)
     otp = models.IntegerField(null=False,blank=False)
     sent_on= models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return ("%s has received otps: %s" %(self.receiver.username,self.otp))
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='user_posts')
+    photo = models.ImageField(upload_to=image_file_path,blank=False,editable=False)
+    text = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    posted_on = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL,related_name="likers",blank=True,symmetrical=False)
+
+    class Meta:
+        ordering = ['-posted_on']
+
+    def number_of_likes(self):
+        if self.likes.count():
+            return self.likes.count()
+        else:
+            return 0
+
+    def __str__(self):
+        return f'{self.author}\'s post'
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post',on_delete=models.CASCADE,related_name='post_comments')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='user_comments')
+    text = models.CharField(max_length=100)
+    posted_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-posted_on']
+
+    def __str__(self):
+        return f'{self.author}\'s comment'
