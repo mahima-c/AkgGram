453b295f8b6480682da7145a5e6fa498dc3b5cea
Mahima gupta <54117469+mahima-c@users.noreply.github.com>
2019-11-06 19:50:40 +0530

    Add files via upload

--- akggram/akggram/urls.py
+++ akggram/akggram/urls.py
@@ -1,10 +1,18 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import refresh_jwt_token
+from rest_framework_jwt.views import verify_jwt_token
+from django.conf.urls import url
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('insta/', include('insta.urls')),
-    #path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-
+    path('api-auth/', include('rest_framework.urls')),
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-refresh/', refresh_jwt_token),
+    url(r'^api-token-verify/', verify_jwt_token),
 ]
